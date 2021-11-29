59c067fa208b662b3dff2a883974ceb016ba2a6b
rohankumardubey <rohankumardubey497@gmail.com>
2021-11-29 17:52:54 +0530

    updated
    made java docs update

--- src/main/java/com/company/MultithreadFolderCleaner/CleaningScheduler.java
+++ src/main/java/com/company/MultithreadFolderCleaner/CleaningScheduler.java
@@ -1,5 +1,7 @@
 package com.company.MultithreadFolderCleaner;
 
+import java.io.File;
+
 public class CleaningScheduler implements Runnable {
     @Override
     public void run() {
--- src/main/java/com/company/PropertiesLogger/loggerPropertiesSample.java
+++ src/main/java/com/company/PropertiesLogger/loggerPropertiesSample.java
@@ -1,25 +1,25 @@
-package com.company.PropertiesLogger;
-
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-
-public class loggerPropertiesSample {
-    public static void main(String[] args) throws IOException {
-        Properties sample = new Properties();
-        InputStream in = loggerPropertiesSample.class.getClassLoader().getResourceAsStream("log4j2.properties");
-        sample.load(in);
-        System.out.println(sample.getProperty("log4j.appender.consoleAppender.layout"));
-
-        Logger logger = LogManager.getLogger();
-        logger.debug("Debug Message Logged !!!");
-        logger.info("Info Message Logged !!!");
-        logger.error("Error Message Logged !!!", new NullPointerException("NullError"));
-    }
-}
-
+//package com.company.PropertiesLogger;
+//
+//
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.util.Properties;
+//
+//
+//public class loggerPropertiesSample {
+//    public static void main(String[] args) throws IOException {
+//        Properties sample = new Properties();
+//        InputStream in = loggerPropertiesSample.class.getClassLoader().getResourceAsStream("log4j2.properties");
+//        sample.load(in);
+//        System.out.println(sample.getProperty("log4j.appender.consoleAppender.layout"));
+//
+//        Logger logger = LogManager.getLogger();
+//        logger.debug("Debug Message Logged !!!");
+//        logger.info("Info Message Logged !!!");
+//        logger.error("Error Message Logged !!!", new NullPointerException("NullError"));
+//    }
+//}
+//
