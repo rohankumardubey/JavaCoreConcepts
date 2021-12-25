0743cee1a132cf24b7d21dbc62226b10b647b1e3
rohankumardubey <rohankumardubey497@gmail.com>
2021-12-25 15:42:22 +0530

    updates for java coding challenge

--- src/main/java/com/company/JavCodingChallanges/PasswordComplexity.java
+++ src/main/java/com/company/JavCodingChallanges/PasswordComplexity.java
@@ -3,12 +3,19 @@ package com.company.JavCodingChallanges;
 public class PasswordComplexity {
     public static void main(String[] args) {
         System.out.println(isPasswordComplex("rohan"));
+        System.out.println(isPasswordComplexSample("rohan"));
     }
 
+    //method 1
     private static Boolean isPasswordComplex(String password){
         return password.length() >= 6 && !(password.contentEquals(password.toUpperCase())) && !(password.contentEquals(password.toLowerCase())) && (password.contains("0") ||
                 password.contains("1") || password.contains("2") || password.contains("3") ||
                 password.contains("4") || password.contains("5") || password.contains("6") ||
                 password.contains("7") || password.contains("8") || password.contains("9"));
+    }
+
+    //method 2
+    private static Boolean isPasswordComplexSample(String password){
+        return password.length() >=6 && password.matches(".*\\d.*") && password.matches(".*[a-z].*") && password.matches(".*[A-Z].*");
     }
 }
